4dcddadebb281956777d9dead237ce095334ef77
James Kulba <James.Kulba@thermofisher.com>
2017-05-11 15:51:25 -0700

    minor tweaks

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -7,10 +7,22 @@ import { HelpComponent } from './help/help.component';
 import { AdminComponent } from './admin/admin.component';
 
 export const router: Routes = [
-    {path: '',  redirectTo: 'about', pathMatch: 'full'},
-    {path: 'about', component: AboutComponent},
-    {path: 'help', component: HelpComponent},
-    {path: 'admin', component: AdminComponent}
+    {
+		path: '',
+		redirectTo: '/about',
+		pathMatch: 'full'
+	},
+    {
+		path: 'about',
+		component: AboutComponent
+	},
+    {
+		path: 'help',
+		component: HelpComponent
+	},
+    {
+		path: 'admin',
+		component: AdminComponent}
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
